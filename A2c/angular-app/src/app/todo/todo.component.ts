bddf06f67d81a3e7fb61b312a30ff4b195c71fe0
Pritam Jatkar <pritamjatkar1724@gmail.com>
2025-04-10 12:28:25 +0530

    A2c

---
+++ A2c/angular-app/src/app/todo/todo.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+  todos: string[] = [];
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.todos.push(this.newTodo.trim());
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+}
